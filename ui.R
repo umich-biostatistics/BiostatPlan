38967c06f0b961e4c1d6174a4b1cc70559de8307
Kleinsasser <mkleinsa@UMICH.EDU>
2019-11-25 11:06:20 -0500

    UI framing

--- ui.R
+++ ui.R
@@ -2,11 +2,51 @@
 ui = fluidPage(
   
   fluidRow(
-    spli
+    headerPanel(h3("Biostatistics M.S. Course Planner", style="color: #02169B")),
+              div(style = "height:55px;background-color: #F1F1F1;") 
+  ),
+  
+  
+  fluidRow(
+    splitLayout(
+      fluidPage(h4("Fall, Year 1"),
+               wellPanel(
+                 submitButton("Add Course")
+               )
+      ),
+      fluidPage(h4("Spring, Year 1"),
+                wellPanel(
+                  submitButton("Add Course")
+                )
+      )
+    )
   ),
   
   fluidRow(
-    
+    splitLayout(
+      fluidPage(h4("Fall, Year 2"),
+                wellPanel(
+                  submitButton("Add Course")
+                )
+      ),
+      fluidPage(h4("Spring, Year 2"),
+                wellPanel(
+                  submitButton("Add Course")
+                )
+      )
+    )
+  ),
+  
+  fluidRow(
+    column(width = 3,
+      wellPanel(
+        submitButton("Submit")
+      )     
+    )
+  ),
+  
+  fluidPage(
+    h4("Report:")
   )
   
 )
